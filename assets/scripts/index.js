9479bbd344cfe6256a895e6197d134e1543a87dc
“Natalia <natalia.rudiger@gmail.com>
2022-02-02 19:22:46 -0200

    passo 3 e 4 completos

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,11 +2,22 @@
 console.log('JS connected'); // <== just a quick check up to make sure js doc is connected properly
 
 
-let wordCountArea = document.getElementById('wordCount-area');
-console.log(wordCountArea);
 
-let wordCountButton = document.getElementById('wordCount-button');
-console.log(wordCountButton);
+const wordCountButton = document.getElementById('wordCount-button');
 
-let wordCountText = document.getElementById('wordCount-text');
-console.log(wordCountText);
+
+
+function wordCount() {
+
+    const wordCountArea = document.getElementById('wordCount-area').value;
+    let count = 0;
+    let split = wordCountArea.split(' ');
+
+    for (let i = 0; i < split.length; i++) {
+        if (split[i] != "") {
+            count ++;
+        }
+    }
+  
+    document.getElementById('wordCount-text').innerHTML = count;
+}
