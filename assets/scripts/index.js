8889d56031be36a505cd3b8775dfb1c662d6887d
“Natalia <natalia.rudiger@gmail.com>
2022-02-02 15:34:53 -0200

    passo 2 completo - seletores

---
+++ assets/scripts/index.js
@@ -0,0 +1,12 @@
+
+console.log('JS connected'); // <== just a quick check up to make sure js doc is connected properly
+
+
+let wordCountArea = document.getElementById('wordCount-area');
+console.log(wordCountArea);
+
+let wordCountButton = document.getElementById('wordCount-button');
+console.log(wordCountButton);
+
+let wordCountText = document.getElementById('wordCount-text');
+console.log(wordCountText);
